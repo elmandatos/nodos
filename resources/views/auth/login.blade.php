dbeba1f2e7ed74ba8671149c8388a3d86258c6a5
MANDATOS <kanakemandatos@gmail.com>
2018-08-28 16:52:41 -0500

    Creado login sin modulo de cerrar sesión ni seguridad

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,10 @@
+@extends("layout")
+@section("contenido")
+    <h1>Login</h1>
+    <form class="" action="/login" method="post">
+        {{ csrf_field() }}
+        <input type="email" name="email" placeholder="Correo">
+        <input type="password" name="password" placeholder="Password">
+        <input type="submit" value="Entrar">
+    </form>
+@stop
