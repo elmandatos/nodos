9af215bb41fd913b13f1ecd5e70f37d3f666947f
Ivan Lopez <fuchi@live.com>
2018-09-17 18:13:29 -0500

    Creacion del controlador de envio de email
    Se creo el controlador para enviar los QR via email

---
+++ app/Http/Controllers/EmailsController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use UsersController;
+use DB;
+use Mail;
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
+
+class EmailsController extends Controller
+{
+    /**
+     * Generacion y envio por EMAIL de los codigos QR
+     */
+    
+    public function sendAllEmails() {
+        $users = DB::table("users")->get();
+        foreach($users as $user) {
+            $qr = QrCode::format('png')->size(399)->generate($user->id, '../public/qrcodes/qrcode_id.png');
+            Mail::send("email.all", 
+            ['qr' => $qr], 
+            function($message) use ($user){
+                $message->from('ivan.lopez3k@gmail.com', 'probando 1 2 3');
+                $message ->attach('../public/qrcodes/qrcode_id.png');
+                $message->to($user->email)->subject('test email');
+            });
+        }      
+    }
+}
