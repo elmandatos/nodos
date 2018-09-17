9452479a11e44d75bd5f0c1a88e442ba498529eb
Ivan Lopez <fuchi@live.com>
2018-09-17 08:48:38 -0500

    Agregado librerias QR y Jquery
    agregada las librerias para la lectura del QR y Jquery para el ajax, se necesita instalar el certificado SSL para el funcionamiento correcto del lector QR

---
+++ public/js/camera.js
@@ -0,0 +1,40 @@
+/**
+ * Crear una instancia de Instacan y desplegar en un div lo que ve la camara
+ * content es el contenido que hay en el codigo QR
+ * en este caso va a ser el ID del usuario.
+ * Si existe el usuario en la BD, entonces nos reedirecionara a su pagina con sus datos
+ * si no existe nos enviara un mensaje de error diciendo que no existe
+ */
+
+let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
+scanner.addListener('scan', function (content) {
+    let id = content;
+    let url = "/users/" + id;
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: () => {
+            window.location.href = url;
+        },
+        error: () => {
+            alert("No existe usuario con id: " + id);
+        }
+    });
+
+});
+
+/**
+ * Obtiene las camaras que hay en la PC, 0 es para la webcam interna
+ * 1 es para la webcam externa
+ */
+Instascan.Camera.getCameras().then(function (cameras) {
+    console.log(cameras);
+
+    if (cameras.length > 0) {
+        scanner.start(cameras[1]);
+    } else {
+        console.error('No cameras found.');
+    }
+}).catch(function (e) {
+    console.error(e);
+});
