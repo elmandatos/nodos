5c53490b36a73d58144a7eb6b8353560dd446de4
Ivan Lopez <fuchi@live.com>
2018-10-02 07:47:14 -0500

    Creado script de webcam
    Se creo un script para pasar la imagen capturada de webcam a un canvas  y esta a una imagen

---
+++ public/js/webcam.js
@@ -0,0 +1,28 @@
+let capture;
+
+function setup() {
+    let canvas = createCanvas(320, 240);
+    canvas.parent("canvasParent");
+    background(0);
+    capture = createCapture(VIDEO);
+    capture.size(320, 240);  
+    capture.hide();                
+}
+
+function draw() {
+    image(capture, 0, 0);
+}
+
+let button = document.getElementById("capturar");
+let img = document.getElementById("desplegar");
+let foto = document.getElementById("foto");
+
+button.addEventListener("click", function () {
+    let canvas = document.getElementById("defaultCanvas0");
+    let dataUrl = canvas.toDataURL("image/png");
+    img.src = dataUrl;
+    let cleanData = dataUrl.replace("data:image/png;base64,", "");
+    cleanData = cleanData.replace(" ", "+");
+    foto.value = cleanData;
+    console.log(foto.value);
+});
