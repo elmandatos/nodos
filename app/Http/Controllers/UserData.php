1cb66048e460b9daa21d1137ae8c1eb3e1cc38f0
Ivan Lopez <fuchi@live.com>
2018-10-04 14:17:59 -0500

    Agregada funcion exporUserHours
    Se agrego una funcion para exportar los usuarios junto con sus horas hechas entre registros

--- app/Http/Controllers/UserData.php
+++ app/Http/Controllers/UserData.php
@@ -6,6 +6,8 @@ use DB;
 use App\User;
 use Illuminate\Http\Request;
 use Excel;
+use Carbon\Carbon;
+
 
 class UserData extends Controller
 {
@@ -36,5 +38,43 @@ class UserData extends Controller
         });
 
         return redirect()->route("users.index");
+    }
+
+    public function exportUsersHours()
+    {
+        \Excel::create('Users', function($excel) {
+            $hour = DB::table('hours')
+            ->select('hours_id', DB::raw('time(sum(TIMEDIFF(hora_salida, hora_entrada))) as total'))
+            ->whereNotNull('hora_salida')
+            ->groupBy('hours_id');
+
+            $users = DB::table('hours')
+            ->joinSub($hour, 'total', function($join) {
+                $join->on('hours.hours_id', '=', 'total.hours_id');
+            })
+            ->join('users', 'users.id', '=', 'hours.user_id')
+            ->select('users.nombres', 'users.apellidos','hours.fecha', 'hours.hora_entrada', 'hours.hora_salida', 'total')
+             ->orderBy('users.id', 'asc')
+            ->get();
+            $users = json_decode(json_encode($users), true);
+
+            $excel->sheet('Registro de horas', function($sheet) use($users) {
+                $sheet->setColumnFormat(array(
+                    'C'=>'dd/mm/yy',
+                    'D'=>'h:mm:ss',
+                    'E'=>'h:mm:ss',
+                    'F'=>'h:mm:ss'
+                ));
+                
+                $i = 2;
+                foreach($users as $user){
+                    $sheet->setCellValue('F'.$i,'=E'.$i.'-D'.$i);
+                    $i++;
+                }
+                
+                $sheet->setCellValue('F'.$i,'=sum(F2:F'.($i-1).')');
+                $sheet->fromArray($users);
+            });
+        })->export('csv');
     }
 }
