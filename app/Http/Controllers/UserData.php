b103468d4a3d7aa3f324f846c8765420dfa40bb4
Ivan Lopez <fuchi@live.com>
2018-11-11 17:46:13 -0600

    fix export user hours excel
    se arreglo la exportacion de las horas totales del usuario

--- app/Http/Controllers/UserData.php
+++ app/Http/Controllers/UserData.php
@@ -50,35 +50,15 @@ class UserData extends Controller
             $excel = $reader->get();
             // iteracción
             $reader->each(function($row) {
-                // $user = new User;
-                // $user->nombres = $row->nombres;
-                // $user->apellidos = $row->apellidos;
-                // $user->telefono = $row->telefono;
-                // $user->email = $row->email;
-                // $user->matricula = $row->matricula;
-                // $user->carrera = $row->carrera;
-                // $user->rol = $row->tipo;
-                // $user->tipo_de_usuario = "Usuario";
-                // $user->save();
 
                 DB::table("hours")->insertGetId([
                     "user_id" => $row->user_id,
                     "fecha" => $row->fecha,
                     "hora_entrada" => $row->hora_entrada,
-                    "hora_salida" => $row->hora_salida,
-                    
+                    "hora_salida" => $row->hora_salida,              
                 ]);
-
-                
-
-
-
-
-
-
             });
         });
-
         return redirect()->route("users.index");
     }
 
@@ -96,33 +76,44 @@ class UserData extends Controller
             })
             ->join('users', 'users.id', '=', 'hours.user_id')
             ->select('users.nombres', 'users.apellidos','hours.fecha', 'hours.hora_entrada', 'hours.hora_salida', 'total')
-             ->orderBy('users.id', 'asc')
+            ->orderBy('users.id', 'asc')
             ->get();
-            $users = json_decode(json_encode($users), true);
 
+            $users = json_decode(json_encode($users), true);
+            
             $excel->sheet('Registro de horas', function($sheet) use($users) {
                 $sheet->setColumnFormat(array(
                     'C'=>'dd/mm/yy',
                     'D'=>'h:mm:ss',
                     'E'=>'h:mm:ss',
-                    'F'=>'[h]:mm:ss'
+                    'F'=>'h:mm:ss',
+                    'G'=>'[h]:mm:ss'
                 ));
 
                 $i = 2;
                 $totalHorasFormula="=";
 
+                $nombre = $users[0]['nombres'];
                 foreach($users as $user){
-                    $sheet->setCellValue('F'.$i,'=E'.$i.'-D'.$i);
-                    if( $totalHorasFormula == "=" )
-                        $totalHorasFormula.="F$i";
-                    else
-                        $totalHorasFormula.="+F$i";
-                    $i++;
-                }
-
-                $sheet->setCellValue('F'.$i,$totalHorasFormula);
+                    if($user['nombres'] == $nombre){
+                        $sheet->setCellValue('F'.$i,'=E'.$i.'-D'.$i);
+                        if( $totalHorasFormula == "=" ){
+                            $totalHorasFormula.="F$i";
+                        }else{
+                            $totalHorasFormula.="+F$i";
+                            $i++;
+                        }
+                    }else {
+                        $i++;
+                        $sheet->setCellValue('G'.($i-2),"Total:");
+                        $sheet->setCellValue('G'.($i-1),$totalHorasFormula);
+                        $nombre = $user['nombres'];
+                        $totalHorasFormula="=F$i";      
+                    }
+                }     
                 $sheet->fromArray($users);
             });
         })->export('xlsx');
+        
     }
 }
