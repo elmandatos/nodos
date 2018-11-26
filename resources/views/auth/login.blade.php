3968f53a60f683575c3b17808d933519e1e94b67
Alejandro Pérez <alejandrogibran@hotmail.com>
2018-11-26 16:41:54 -0600

    Cambios

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,6 +6,11 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Inicio de Sesion</title>
   <link rel="stylesheet" type="text/css" href="{{asset('css/login.blade.css')}}">
+  <link rel="stylesheet" href="{{asset('css/materialize-icons.css')}}">
+  <!--Import Font Awesome Icon Fonts-->
+  <link rel="stylesheet" href="{{asset('css/fa_css/solid.css')}}">
+  <!--Import materialize.css-->
+  <link rel="stylesheet" type="text/css" href="{{asset('css/materialize.min.css')}}" media="screen,projection" />
 </head>
 <body background="{{asset('fondo.jpg')}}">
   {{-- <h1>Inicio de Sesion</h1> --}}
@@ -20,8 +25,24 @@
             <a href="{{route("home")}}"><img src="../logo_Nodos.svg"></a>
               {{ csrf_field() }}
               <br>
-              <input type="email" name="email" placeholder="Correo"><br>
-              <input type="password" name="password" placeholder="Contraseña"><br><br>
+              {{-- <input type="email" name="email" placeholder="Correo"><br> --}}
+              {{-- <input type="password" name="password" placeholder="Contraseña"><br><br> --}}
+              <div class="row">
+                <div class="input-field col s10">
+                  <i class="material-icons prefix">account_circle</i>
+                  <input id="correo" type="email" class="validate">
+                  <label for="correo">Correo</label>
+                  <span class="helper-text" data-error="Correo no válido"></span>
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="input-field col s10">
+                  <i class="material-icons prefix">lock</i>
+                  <input id="contraseña" type="password">
+                  <label for="contraseña">Contraseña</label>
+                </div>
+              </div>
               <input class="boton" type="submit" value="Iniciar Sesion">
               <br><br>
           </form>
@@ -29,6 +50,8 @@
       </div>
       <div class="columna_costados"></div>
   </div>
-
+  <script src="{{asset('/js/jquery-3.3.1.min.js')}}" type="text/javascript"></script>
+  <script src="{{asset('/js/materialize.min.js')}}" type="text/javascript"></script>
+  <script src="{{asset('/js/material-dialog.min.js')}}" type="text/javascript"></script>
 </body>
 </html>
