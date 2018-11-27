a862965bd4df29dc42ab38c2600fa17ad662cedc
Alejandro Pérez <alejandrogibran@hotmail.com>
2018-11-27 17:35:55 -0600

    Inicio de Sesión estilo Google con Hovers
    - Inputs más cerca uno del otro
    - Boton de iniciar sesion con Hover (->) y efecto de click

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,7 +13,6 @@
   <link rel="stylesheet" type="text/css" href="{{asset('css/materialize.min.css')}}" media="screen,projection" />
 </head>
 <body>
-  {{-- <h1>Inicio de Sesion</h1> --}}
   <div class="cabecera">
   </div>
 
@@ -25,25 +24,22 @@
             <a href="{{route("home")}}"><img src="../logo_Nodos.svg"></a>
               {{ csrf_field() }}
               <br>
-              {{-- <input type="email" name="email" placeholder="Correo"><br> --}}
-              {{-- <input type="password" name="password" placeholder="Contraseña"><br><br> --}}
-              <div class="row">
+              <div class="row" id="margen">
                 <div class="input-field col s11">
                   <i class="material-icons prefix">account_circle</i>
-                  <input id="correo" type="email" class="validate" autofocus>
+                  <input id="correo" type="email" autofocus>
                   <label for="correo">Correo</label>
-                  <span class="helper-text" data-error="Correo no válido"></span>
                 </div>
               </div>
 
-              <div class="row">
+              <div class="row margen" id="margen2">
                 <div class="input-field col s11">
                   <i class="material-icons prefix">lock</i>
                   <input id="contraseña" type="password">
                   <label for="contraseña">Contraseña</label>
                 </div>
               </div>
-              <input class="boton" type="submit" value="Iniciar Sesion">
+              <button class="boton" type="submit"><span>Iniciar Sesión </span></button>
               <br><br>
           </form>
         <div class="columna_costados_interno"></div>
