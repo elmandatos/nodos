0757ef963c54ebf3d2849006244f0a41f3ade4a6
Alejandro Pérez <alejandrogibran@hotmail.com>
2018-12-06 17:18:59 -0600

    V1.7

--- resources/views/usuarios/show.blade.php
+++ resources/views/usuarios/show.blade.php
@@ -71,11 +71,12 @@
           <a class="dropdown-trigger btn" data-target="dropDown1"><i class="material-icons right">arrow_drop_down_circle</i>Acceso</a>
           <!-- Dropdown Structure -->
            <ul id='dropDown1' class='dropdown-content'>
-            @if($statusEntrada>0)
+              @if($statusEntrada==0)
               <li><a href="{{route("get_in",$user->id)}}" class="entrada">Entrada</a></li>
             @else
             <li><a href="{{route("get_out",$user->id)}}" class="salida">Salida</a></li>
-            @endif
+          @endif
+
         </div>
 
         <div class="col s12 l6">
