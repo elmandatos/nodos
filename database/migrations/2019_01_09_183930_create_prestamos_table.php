d423c9e9410cd42d34a1d1fafeed4299b299cfd0
Alejandro Pérez <alejandrogibran@hotmail.com>
2019-01-10 11:08:52 -0600

    Migracion crear tabla prestamos
    id_piezas
    id_usuarios
    cantidad
    Egreso
    Ingreso

---
+++ database/migrations/2019_01_09_183930_create_prestamos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePrestamosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prestamos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('id_usuario');
+            $table->unsignedInteger('id_piezas');
+            $table->unsignedInteger('cantidad');
+            $table->timestamp('hora_egreso')->nullable();
+            $table->timestamp('hora_ingreso')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prestamos');
+    }
+}
