1c008bcd13a26e9dbc374f8007b14c43702b8d11
Alejandro Pérez <alejandrogibran@hotmail.com>
2019-01-13 15:44:58 -0600

    Boton "atrás" y Validacion en Login
    - Se concreto la implementación del botón de atrás en la vista Login. Ahora regresa a la ultima ruta visitada, ignorando las veces que refresques la pagina o regreses al login por problemas de validación, siempre regresa a la ultima ruta que no sea la misma vista "login"
    
    - Ahora al ocurrir un error de validación, se despliega en pantalla dicho error para informar al usuario de lo ocurrido

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,33 +13,39 @@
   <link rel="stylesheet" href="{{asset('css/materialize-icons.css')}}">
 </head>
 <body>
-  <div class="cabecera"></div>
-
   <div class="fila">
     <div class="columna_costados"></div>
       <div class="columna_principal">
         <div class="right-align columna_costados_interno">
-           <a class="btn-floating btn-large waves-effect waves-light" href="{{ url()->previous() }}">
-            <i class="material-icons">arrow_back</i>
-          </a>
         </div>
           <form class="formulario" action="/login" method="POST">
+            @php 
+              if( url()->previous() != "https://nodos.test/login" )
+              {
+                  Session::put('prevURL', url()->previous());
+              }
+              $previousUrl = Session::get('prevURL');
+            @endphp
+            <a class="btn-floating btn-large waves-effect waves-light" id="btnBack" href="{{$previousUrl}}">
+              <i class="material-icons">arrow_back</i>
+            </a>
             <a href="{{route("home")}}"><img src="../logo_Nodos.svg"></a>
               {{ csrf_field() }}
-              <br>
               <div class="row" id="margen">
                 <div class="input-field col s11">
                   <i class="material-icons prefix">account_circle</i>
-                  <input id="email" name="email" type="email" autofocus>
                   <label for="email">Correo</label>
+                  <input id="email" name="email" type="email" autofocus>
+                  {!! $errors->first("email", "<span class='red-text'>:message</span>")!!}
                 </div>
               </div>
 
               <div class="row margen" id="margen2">
                 <div class="input-field col s11">
                   <i class="material-icons prefix">lock</i>
-                  <input id="password" name="password" type="password">
                   <label for="password">Contraseña</label>
+                  <input id="password" name="password" type="password">
+                  {!! $errors->first("password", "<span class='red-text'>:message</span>")!!}
                 </div>
               </div>
               <button class="boton" type="submit"><span>Iniciar Sesión </span></button>
